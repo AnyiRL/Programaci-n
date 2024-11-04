0d9bf318beea00db9eac89c3ecc0f57276d38c73
AnyiRL <ruananyi7@gmail.com>
2024-11-04 14:01:15 +0100

    .

---
+++ DEIM/Assets/Scripts/IA/SeeAction.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HearAction (A)", menuName = "ScriptableObject/Action/HearAction")]
+
+public class SeeAction : Action
+{
+    public override bool Check(GameObject owner)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void DrawGizmos(GameObject owner)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
